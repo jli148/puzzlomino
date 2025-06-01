93a7183ecc6edc54482a439d78e6df87e3f5ddff
Jax Li <jaxli148@gmail.com>
2025-06-01 02:37:23 -0400

    add puzzlomino to a Discord bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import asyncio
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents(messages=True, message_content=True)
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            # cut off the .py from the file name
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(os.getenv("TOKEN"))
+
+
+asyncio.run(main())
